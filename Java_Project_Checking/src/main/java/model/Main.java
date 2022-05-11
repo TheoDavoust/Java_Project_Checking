5096b3e3b03da57074a1541c8e721dccffabd04f
Theo Davoust <theo.davoust@etu.univ-tours.fr>
2022-05-11 16:47:26 +0200

    Test Main, Checking creation

---
+++ Java_Project_Checking/src/main/java/model/Main.java
@@ -0,0 +1,19 @@
+package model;
+
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		
+		JButton b = new JButton("Test");
+		b.setBounds(130, 100, 100, 40);
+		
+		f.add(b);
+		
+		f.setSize(400, 500);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+}
