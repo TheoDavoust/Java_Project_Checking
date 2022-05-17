e2f4a5b059c1d9c6b75a133f317216fcc82ef518
Freeze <2lj.fzb@gmail.com>
2022-05-17 11:29:06 +0200

    Correction de l'arrêt du processus lorsque l'on ferme la fenêtre swing.

--- Java_Project_Checking/src/main/java/model/Main.java
+++ Java_Project_Checking/src/main/java/model/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton b = new JButton("Test");
 		b.setBounds(130, 100, 100, 40);
