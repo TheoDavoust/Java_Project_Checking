7c50e744d319ceee8714b29ae777bebf4e7134d3
Theo Davoust <theo.davoust@etu.univ-tours.fr>
2022-05-30 15:28:45 +0200

    Vue de l'émulateur, début

---
+++ Java_Project_Checking/src/main/java/view/ButtonInput.java
@@ -0,0 +1,12 @@
+package view;
+
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+
+public class ButtonInput extends JButton{
+	public ButtonInput() {
+		super();
+		this.setText("Default");
+	}
+}
---
+++ Java_Project_Checking/src/main/java/view/TextInput.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.awt.Dimension;
+
+import javax.swing.JTextField;
+
+public class TextInput extends JTextField{
+	public TextInput() {
+		super();
+	}
+}
